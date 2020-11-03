12b8f7d8fdbe6a2434de154aef693dceb1e89228
youssef chaker <youssef.chaker@etudiant-isi.utm.tn>
2020-11-03 17:02:20 +0100

    setting up the view

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const app = express();
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
-
+app.use(express.static('views'));
+app.use(express.json());
 // view engine
 app.set('view engine', 'ejs');
 
 // routes
 app.get('/', (req, res) => res.render('home'));
-
 app.listen(3000);
 console.log('we are listening to port 3000');
 
